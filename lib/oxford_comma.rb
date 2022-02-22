e6b724b45777125e1fad27241bf840ea393f24f9
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-02-21 22:59:50 -0500

    commit tree 6578a215bed3c39bf1353171d5a22cda5545c21d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    # binding.pry
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length > 2 
+        all_but_last_value = array[0, array.length-1].join(", ")
+        all_but_last_value << ", and #{array[array.length-1]}"
+    else 
+        array.join
+    end
 end
+
+arr1 = ["hello"]
+arr2 = ["hello", "world"]
+arr3 = ["hello", "world", "g'day"]
+arr4 = ["hello", "world", "g'day", "mate"]
+
+# puts oxford_comma arr1
+# puts oxford_comma arr2
+# puts oxford_comma arr3
+puts oxford_comma arr4
